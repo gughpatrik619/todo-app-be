b8ad16155306d101384d41ca5019bb41f23bb0aa
Patrik Gúgh <gugh.patrik.it@gmail.com>
2021-01-17 01:13:42 +0100

    Minor fixes

--- src/main/kotlin/com/example/todoapp/security/JwtUtils.kt
+++ src/main/kotlin/com/example/todoapp/security/JwtUtils.kt
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Service
+import java.time.Instant
 import java.util.*
 
 @Service
@@ -14,17 +15,17 @@ class JwtUtils(
     private val jwtSecret: String,
 
     @Value("\${todo.app.jwt.expiration_ms}")
-    private val jwtExpirationMs: Int
+    private val jwtExpirationMs: Long
 ) {
     fun generateJwtToken(authentication: Authentication): String =
         Jwts.builder()
             .setSubject((authentication.principal as UserDetailsImpl).username)
-            .setIssuedAt(Date())
-            .setExpiration(Date(Date().time + jwtExpirationMs))
+            .setIssuedAt(Date.from(Instant.now()))
+            .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationMs)))
             .signWith(SignatureAlgorithm.HS512, jwtSecret)
             .compact()
 
-    fun getUserNameFromJwtToken(token: String?): String =
+    fun getUserNameFromJwtToken(token: String): String =
         Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).body.subject
 
     fun validateJwtToken(authToken: String) =
--- src/main/kotlin/com/example/todoapp/service/UserDetailsServiceImpl.kt
+++ src/main/kotlin/com/example/todoapp/service/UserDetailsServiceImpl.kt
@@ -1,6 +1,5 @@
 package com.example.todoapp.service
 
-import com.example.todoapp.model.User
 import com.example.todoapp.model.UserDetailsImpl
 import com.example.todoapp.repository.UserRepository
 import org.springframework.security.core.userdetails.UserDetails
@@ -8,15 +7,14 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.util.*
 
 @Service
 class UserDetailsServiceImpl(private val userRepository: UserRepository) : UserDetailsService {
 
     @Transactional(readOnly = true)
     override fun loadUserByUsername(username: String): UserDetails {
-        val userOptional = userRepository.findByUsername(username)
-        val user = userOptional.orElseThrow { UsernameNotFoundException("No user found with username $username") }
+        val user = userRepository.findByUsername(username)
+            .orElseThrow { UsernameNotFoundException("No user found with username $username") }
         return UserDetailsImpl.build(user)
     }
 }
