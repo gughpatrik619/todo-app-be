8218a6ab6591ec27dcd358c9dc6dfa633b962dc2
Patrik Gúgh <gugh.patrik.it@gmail.com>
2021-03-15 17:22:28 +0100

    Fixes

--- src/main/kotlin/com/example/todoapp/service/TodoService.kt
+++ src/main/kotlin/com/example/todoapp/service/TodoService.kt
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
+//@Transactional
 class TodoService(
     private val todoRepository: TodoRepository,
     private val userRepository: UserRepository
